e88c598cce224fb1701e729c881d341d0572d882
natepsacc <natepsacc@gmail.com>
2026-01-06 16:06:08 -0500

    Concurrency

---
+++ concurrency/concurrency.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)	
+
+func fetch(varName string, c chan string) {
+
+	fmt.Println("fetch called")
+
+	resp, err := http.Get(varName)
+	if err != nil {
+		log.Fatal("Error making GET: ", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		//String print formatted 
+		c <- fmt.Sprintf("error %d for URL %s", resp.StatusCode, varName)
+
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("error reading body: ", err)
+	}
+
+	c <- string(body)	
+}
+
+func main() {
+	var urlArr [2]string // create string with a length of 2
+
+	// assign to our array
+	urlArr[0] = "https://example.com"
+	urlArr[1] = "https://example.com/example"
+	
+	fmt.Println(urlArr)
+	
+	//create channel.  Creating channel inside of loop will make is synchronous as we would be waiting to assign the response after request
+	//so we couldnt loop(makeChannel go makeRequest var <- channelResponse) as the var <- channelResponse would hang until we got the response
+	channel := make(chan string)
+
+	for i := 0; i < len(urlArr); i++ {
+		go fetch(urlArr[i], channel)
+	}
+	
+
+	for i := 0; i < len(urlArr); i++ {
+		fmt.Println(<-channel)
+	}
+}
+
+
