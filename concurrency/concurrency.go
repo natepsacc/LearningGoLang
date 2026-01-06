faef869d24e952886c7877b4e1051f8cf6da5f54
natepsacc <natepsacc@gmail.com>
2026-01-06 16:16:50 -0500

    moveFetchTonewFile

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -7,29 +7,6 @@ import (
 	"net/http"
 )	
 
-func fetch(varName string, c chan string) {
-
-	fmt.Println("fetch called")
-
-	resp, err := http.Get(varName)
-	if err != nil {
-		log.Fatal("Error making GET: ", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		//String print formatted 
-		c <- fmt.Sprintf("error %d for URL %s", resp.StatusCode, varName)
-
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal("error reading body: ", err)
-	}
-
-	c <- string(body)	
-}
 
 func main() {
 	var urlArr [2]string // create string with a length of 2
